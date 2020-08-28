596d22da0c95a3b531d8c25b72f0a0b7017bf730
NPER2012 <windows2012xiao@foxmail.com>
2020-08-29 04:54:32 +0800

    scss added

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,41 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
+
+$text-color-highlight: #42b983;
+$text-color-base: #2c3e50;
+
+*{padding: 0; margin: 0;}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+  /*字体抗锯齿渲染 */
+  -webkit-font-smoothing: antialiased;/*chrome、safari*/
+  -moz-osx-font-smoothing: grayscale;/*firefox*/
+  
   text-align: center;
-  color: #2c3e50;
+  color: $text-color-base;
+
+  a {
+    text-decoration: none;
+    color: $text-color-base;
+  }
 }
 
 #nav {
   padding: 30px;
+
+  a{
+    font-weight: bold;
+
+    &.router-link-exact-active {
+      color: $text-color-highlight;
+    }
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
